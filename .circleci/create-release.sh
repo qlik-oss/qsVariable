722e9076ee4c0c713abe2cb835d7dd686913f2ff
Philip Olsén <pol@qlik.com>
2018-11-13 10:51:27 +0100

    updated auto release for github

--- .circleci/create-release.sh
+++ .circleci/create-release.sh
@@ -4,6 +4,11 @@ set -o errexit
 echo "Creating release for version: $PACKAGE_VERSION"
 echo "Artifact name: ./dist/${3}_${PACKAGE_VERSION}.zip"
 
+echo "1: ${1}"
+echo "2: ${2}"
+echo "3: ${3}"
+echo "4: ${4}"
+
 LATEST_VERSION=$(curl --silent "https://api.github.com/repos/$1/$2/releases/latest" | 
   grep '"tag_name":' |                                                                
   sed -E 's/.*"([^"]+)".*/\1/')
@@ -12,7 +17,7 @@ echo "LATEST_VERSION: ${LATEST_VERSION}"
 
 if [ "${LATEST_VERSION}" != "${PACKAGE_VERSION}" ]; then
 
-$HOME/bin/ghr -t ${ghoauth} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -c ${CIRCLE_SHA1} -delete ${PACKAGE_VERSION} "./dist/${3}_${4}.zip"
+$HOME/bin/ghr -t ${ghoauth} -u ${1} -r ${2} -c ${CIRCLE_SHA1} -delete ${PACKAGE_VERSION} "./dist/${3}_${4}.zip"
 
 else
 
