650f85b91f505fa8233132636a8ac0bf36e34e67
Philip Olsén <pol@qlik.com>
2019-01-10 08:29:58 +0100

    keep license on uglify

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
+var saveLicense = require('uglify-save-license');
 var pkg = require('./package.json');
 
 var DIST = './dist',
@@ -44,7 +45,7 @@ gulp.task('qext', function () {
 			cwd: '',
 			base: '',
 			path: NAME + '.qext',
-			contents: new Buffer(JSON.stringify(qext, null, 4))
+			contents: Buffer.from(JSON.stringify(qext, null, 4))
 		}));
 		this.push(null);
 	};
@@ -75,7 +76,11 @@ gulp.task('build',['clean', 'qext', 'less'], function () {
 	gulp.src([SRC + '/**/*.css', SRC + '/**/*.png'])
 		.pipe(gulp.dest(DIST));
 	return gulp.src(SRC + '/**/*.js')
-		.pipe(uglify())
+		.pipe(uglify({
+			output: {
+				comments: saveLicense
+			}
+		}))
 		.pipe(gulp.dest(DIST));
 });
 
