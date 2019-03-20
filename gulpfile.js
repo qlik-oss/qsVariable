540dd8464966488966ba3de9507554c8707aff62
Albert Backenhof <EXT_BPD@qlik.com>
2019-03-20 07:18:36 +0100

    Auto-release on merge to master

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,14 @@ var pkg = require('./package.json');
 var DIST = './dist',
 	SRC = './src',
 	NAME = pkg.name,
-	DEPLOY = process.env.HOMEDRIVE + process.env.HOMEPATH + '/Documents/Qlik/Sense/Extensions/' + NAME;
+	VERSION = process.env.VERSION || 'local-dev';
 
 gulp.task('qext', function () {
 	var qext = {
 		name: 'Variable input',
 		type: 'visualization',
 		description: pkg.description,
-		version: pkg.version,
+		version: VERSION,
 		icon: 'control',
 		preview: 'preview.png',
 		keywords: 'qlik-sense, visualization',
@@ -88,18 +88,8 @@ gulp.task('zip', ['build'], function () {
 	var zip = require('gulp-zip');
 
 	return gulp.src(DIST + '/**/*')
-		.pipe(zip(`${NAME}_${pkg.version}.zip`))
+		.pipe(zip(`${NAME}_${VERSION}.zip`))
 		.pipe(gulp.dest(DIST));
-});
-
-gulp.task('debug', ['clean', 'qext', 'less'], function () {
-	return gulp.src([SRC + '/**/*.css', SRC + '/**/*.png', SRC + '/**/*.js', DIST + '/**/*.qext'])
-		.pipe(gulp.dest(DEPLOY));
-});
-
-gulp.task('deploy', ['build'], function () {
-	return gulp.src(DIST + '/**/*')
-		.pipe(gulp.dest(DEPLOY));
 });
 
 gulp.task('default', ['build']);
