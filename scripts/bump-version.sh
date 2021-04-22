0c641e7c2dd8587a421ac58fa7393ea795519024
Purwa Shrivastava <atq@SELUN-ATQ-MAC.local>
2021-04-22 09:15:56 +0200

    Patch version correction.

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,7 +5,7 @@ join_by () {
   local IFS="$1"; shift; echo "$*";
 }
 
-if [ "${CIRCLE_BRANCH}" == "- release/6.12.x" ]; then
+if [ "${CIRCLE_BRANCH}" == "release/6.12.x" ]; then
   # get version from repo
   OLD_VERSION="$(scripts/get-latest-version.sh $1 $2)"
   echo "Latest GitHub release version: ${OLD_VERSION}"
